e5c35cdaad0d430ef9bd8d2f8f6f4685da5c1265
Benjamin Kröger <benjamin.kroeger@gis-ag.com>
2017-09-26 12:12:47 +0200

    merge develop into master
    commit b628823b7878831a49ecb697a277b4c2efa4888b
    Author: Benjamin Kröger <benjamin.kroeger@gis-ag.com>
    Date:   Tue Sep 26 12:12:13 2017 +0200
    
        1.0.0
    
    commit a286864363371d48b02a9b3075c9c55146bcc63c
    Author: Benjamin Kröger <benjamin.kroeger@gis-ag.com>
    Date:   Tue Sep 26 12:04:35 2017 +0200
    
        updates dependencies
    
    commit f6489c565e2520d4a81af6c89dcd028edae0b9ee
    Author: Benjamin Kröger <benjamin.kroeger@gis-ag.com>
    Date:   Tue Sep 26 12:03:29 2017 +0200
    
        removes index.js from package.json/files
    
    commit 4cfb4a9a0334535ede1663910bbe71911a946293
    Author: Benjamin Kröger <benjamin.kroeger@gis-ag.com>
    Date:   Tue Sep 26 12:03:15 2017 +0200
    
        allows carret range for dependencies
    
    commit e7921bc02b86b04c91cf92587bf8b64260dbc841
    Author: Benjamin Kröger <benjamin.kroeger@gis-ag.com>
    Date:   Tue Sep 26 12:02:48 2017 +0200
    
        changes description in readme
    
    commit 2944f4e481c6913d9b4f261b00d9122fcd4dc41a
    Author: Benjamin Kröger <benjamin.kroeger@gis-ag.com>
    Date:   Tue Sep 26 12:02:36 2017 +0200
    
        updates license (now MIT)
    
    commit 17b1f338b3d1add416408d83fdcd923ffcfbe1b8
    Author: Benjamin Kröger <benjamin.kroeger@gis-ag.com>
    Date:   Tue Sep 26 12:00:23 2017 +0200
    
        fixes linting
    
    commit 69efe7c8ce585d3881b0f6b67d25ae67d89532cc
    Author: Benjamin Kröger <benjamin.kroeger@gis-ag.com>
    Date:   Tue Sep 26 11:59:40 2017 +0200
    
        removes dotenv
    
    commit 1b0ff383fb6f921c5299364f7fce30cba5a09c83
    Author: ajevtic <ajevtic@ztech.io>
    Date:   Thu Aug 24 15:05:35 2017 +0200
    
        update startDate and endDate date fields
    
    commit 97e01774e49dbb411f27c7297dd1e32bfb99580c
    Author: ajevtic <ajevtic@ztech.io>
    Date:   Thu Aug 17 18:54:03 2017 +0200
    
        bugs fixed
        tests added
    
    commit c2076b5471bb0af69922d694eecc56494f13dcce
    Author: ajevtic <ajevtic@ztech.io>
    Date:   Thu Aug 17 16:54:41 2017 +0200
    
        methods and parsers added
    
    commit 228b30c425a710e336cc39df9d244e9f0132dfc5
    Author: ajevtic <ajevtic@ztech.io>
    Date:   Thu Aug 17 15:45:35 2017 +0200
    
        init commit

---
+++ test/index.test.js
@@ -0,0 +1,112 @@
+'use strict';
+
+// node core modules
+
+// 3rd party modules
+import test from 'ava';
+import _ from 'lodash';
+
+// internal modules
+import communityEventsService from '../lib';
+import { mock, record, persist } from './fixtures/http-mocking';
+
+const { unmocked, username, password } = process.env;
+
+test.before(() => (unmocked ? record() : mock()));
+test.after(() => unmocked && persist());
+
+test.beforeEach((t) => {
+  const serviceOptions = {
+    defaults: {
+      authType: 'basic',
+    },
+  };
+  if (unmocked) {
+    Object.assign(serviceOptions.defaults, {
+      auth: {
+        user: username,
+        pass: password,
+      },
+    });
+  }
+  const baseProps = ['id', 'title', 'communityUuid', 'source', 'links', 'author'];
+  const userProps = ['name', 'userId', 'isExternal', 'email', 'userState'];
+  const sourceProps = ['id', 'title', 'self'];
+
+  const eventsProps = {
+    firstLvlProps: [...baseProps, 'published', 'summary', 'eventUuid', 'eventInstUuid', 'repeats', 'location',
+      'parentEvent', 'followed', 'attended', 'tags', 'contributor', 'allday', 'endDate', 'startDate', 'updated'],
+    userProps,
+    linksProps: ['self', 'alternate', 'parentevent', 'attend', 'follow', 'attendees', 'container'],
+    sourceProps: [...sourceProps, 'edit'],
+  };
+  const attendeesProps = {
+    firstLvlProps: [...baseProps, 'role'],
+    userProps,
+    linksProps: ['self', 'container'],
+    sourceProps,
+  };
+
+  const service = communityEventsService('https://apps.na.collabserv.com/communities', serviceOptions);
+  _.assign(t.context, {
+    service,
+    serviceOptions,
+    eventsProps,
+    attendeesProps,
+  });
+});
+
+test.cb('validate retrieving community events => calendarUuid & startDate provided', (t) => {
+  const { service, eventsProps: { firstLvlProps, userProps, linksProps, sourceProps } } = t.context;
+
+  const query = {
+    calendarUuid: '5dd83cd6-d3a5-4fb3-89cd-1e2c04e52250',
+    startDate: '2017-01-04T20:32:31.171Z',
+  };
+
+  service.events(query, {}, (err, events) => {
+    t.ifError(err);
+    t.true(_.isArray(events));
+    events.forEach((event, i) => {
+      firstLvlProps.forEach(prop => t.true(prop in event, `[${prop}] should be a member of events[${i}]`));
+      const { author, contributor, links, source, tags, allday, published, updated, startDate, endDate } = event;
+
+      [author, contributor, links, source].forEach(elem => t.true(_.isPlainObject(elem)));
+      t.true(_.isArray(tags));
+      t.true(_.isBoolean(allday));
+      t.true(_.isDate(new Date(published)));
+      t.true(_.isDate(new Date(updated)));
+      t.true(_.isDate(new Date(startDate)));
+      t.true(_.isDate(new Date(endDate)));
+
+      userProps.forEach(prop => t.true(prop in author, `[${prop}] should be a member of event.author`));
+      userProps.forEach(prop => t.true(prop in contributor, `[${prop}] should be a member of event.contributor`));
+      linksProps.forEach(prop => t.true(prop in links, `[${prop}] should be a member of event.links`));
+      sourceProps.forEach(prop => t.true(prop in source, `[${prop}] should be a member of event.source`));
+    });
+    t.end();
+  });
+});
+test.cb('validate retrieving events attendees => eventInstUuid provided', (t) => {
+  const { service, attendeesProps: { firstLvlProps, userProps, linksProps, sourceProps } } = t.context;
+
+  const query = {
+    eventInstUuid: '2c688d78-5a78-42b2-a2dd-bd5f5493fdc2',
+  };
+
+  service.attendees(query, {}, (err, attendees) => {
+    t.ifError(err);
+    t.true(_.isArray(attendees));
+
+    attendees.forEach((attendee, i) => {
+      firstLvlProps.forEach(prop => t.true(prop in attendee, `[${prop}] should be a member of attendees[${i}]`));
+      const { author, links, source } = attendee;
+      [author, links, source].forEach(elem => t.true(_.isPlainObject(elem)));
+
+      userProps.forEach(prop => t.true(prop in author, `[${prop}] should be a member of attendee.author`));
+      linksProps.forEach(prop => t.true(prop in links, `[${prop}] should be a member of attendee.links`));
+      sourceProps.forEach(prop => t.true(prop in source, `[${prop}] should be a member of attendee.source`));
+    });
+    t.end();
+  });
+});
